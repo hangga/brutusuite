d3f5b4373d73f59b83fbe6c935ef7ab8e5aee68a
Hangga Aji Sayekti <bazeniancode@gmail.com>
2026-07-02 13:33:07 +0700

    Brutu Suite

--- devtools.js
+++ devtools.js
@@ -1,7 +1,7 @@
 // Buat panel di DevTools
 chrome.devtools.panels.create(
-  "API Slurp",           // Judul panel
-  "icons/icon16.png",    // Icon (opsional, bisa gunakan path relatif)
+  "Brutu Suite",           // Judul panel
+  "icons/icon-16.png",    // Icon (opsional, bisa gunakan path relatif)
   "panel.html",          // Halaman konten panel
   function(panel) {
     console.log("Panel API Slurp created");
